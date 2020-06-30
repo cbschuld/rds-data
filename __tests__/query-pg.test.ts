40b8a3da28d9467ead532fbeecbc68a901d96b52
Mark Herhold <mark.herhold@gmail.com>
2020-06-30 17:35:06 -0400

    adding boolean test

--- __tests__/query-pg.test.ts
+++ __tests__/query-pg.test.ts
@@ -10,6 +10,12 @@ test('select an integer', async () => {
   expect(results.data[0].id.number).toBe(1);
 });
 
+test('select a boolean', async () => {
+  const rds = setupRDSDatabase().getInstance();
+  const results = await rds.query('SELECT 1 > 0 AS value');
+  expect(results.data[0].value.boolean).toBe(true);
+});
+
 test('select a uuid', async () => {
   const rds = setupRDSDatabase().getInstance();
   await rds.query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
