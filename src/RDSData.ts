185b84f6deaa8bd25f6d9fd7f76eead1297c3d8c
Luke Becker <luke.becker@fhr.com>
2021-01-29 13:21:30 -0600

    fixing decimals getting stored as whole numbers

--- src/RDSData.ts
+++ src/RDSData.ts
@@ -22,7 +22,7 @@ export interface RDSDataType {
     [x: string]: string | boolean | Buffer | null;
   };
 }
-export type RDSDataTypes = 'stringValue' | 'booleanValue' | 'longValue' | 'isNull' | 'blobValue' | undefined;
+export type RDSDataTypes = 'stringValue' | 'booleanValue' | 'longValue' | 'isNull' | 'blobValue' | 'doubleValue' |undefined;
 
 export type RDSDataParameterValue = string | Buffer | boolean | number | null | undefined;
 export interface RDSDataParameters {
@@ -122,6 +122,9 @@ export class RDSData {
         return 'booleanValue';
       }
       if (t === 'number') {
+        if (val && val as number % 1 !== 0) {
+          return 'doubleValue';
+        }
         return 'longValue';
       }
       if (val === null) {
